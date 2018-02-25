2fff43eacf78c17048fc359a46d1b84a0fbe53ce
IvanBorislavovDimitrov <starstrucks1997@gmail.com>
2018-02-25 17:58:44 +0200

    add

---
+++ Database Introduction - Exercise/queries.sql
@@ -0,0 +1,13 @@
+UPDATE `school`.`students` SET f_num=f_num+1 WHERE `id`;
+
+UPDATE students
+	SETschoolschoolteacher
+		first_name='Mitko'
+	WHERE `f_num`='121216000';
+
+DELETE FROM students WHERE `f_num`='121216000';
+
+	SELECT id, first_name, second_name, third_name, f_num, class_id
+	FROM students
+	WHERE id<4
+	ORDER BY id DESC;
